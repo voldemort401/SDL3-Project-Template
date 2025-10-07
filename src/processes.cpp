e587252224d9d38f55d388ff1976a514eefc2205
voldemort401 <181987124+voldemort401@users.noreply.github.com>
2025-10-07 19:33:29 +0545

    Change the background color to blue by default

--- src/processes.cpp
+++ src/processes.cpp
@@ -6,7 +6,7 @@ void processes::outside_main_loop(SDL_Window* window, SDL_Renderer* renderer){
   circle = {300,300,4};
 }
 void processes::process(double delta, SDL_Window* window, SDL_Renderer* renderer){
-  setbg_color(renderer, 200, 255, 200, 255);
+  setbg_color(renderer, 0, 0, 255, 255);
   create_circle(circle, renderer);
 };
 void processes::physics(double delta, SDL_Window* window, SDL_Renderer* renderer){
